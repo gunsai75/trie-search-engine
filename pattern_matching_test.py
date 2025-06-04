c91b579dcf00517e3c0c35bdb46f09ce31ab5494
Sai Ganesha <saiiiigm1509@gmail.com>
2025-06-04 20:58:51 +0400

    Some new code

--- pattern_matching_test.py
+++ pattern_matching_test.py
@@ -1,4 +1,5 @@
-from typing import List, Tuple, Dict
+from typing import List, Dict
+
 
 class PatternMatch:
     def search_brute_force(self, text: str, pattern: str) -> List[int]:
@@ -96,5 +97,58 @@ class PatternMatch:
             'match-type': 'kmp',
             'pattern': pattern,
             'matches': results_kmp,
-            'matches-count': len(results_kmp)
+            'matches_count': len(results_kmp)
+        }
+    
+    # def search_in_documents(self, pattern: str, relevant_docs: List[str] = None) -> Dict:
+    #     results = {}
+    #     total_matches = 0
+    #     docs_to_search = relevant_docs
+    #     docs_map = test1.map_documents()
+    #     for doc_name in docs_to_search:
+    #         if doc_name in document_texts:
+    #             doc_result = self.search_kmp(get_text_doc, pattern) 
+	# 			result = doc_result
+	# 			total_matches += doc_result['matches_count']
+    #     return {
+    #         'pattern': pattern,
+    #         'total_matches': total_matches,
+    #         'documents_found': len(results),
+    #         'document_results': result
+    #     }
+
+    def search_in_documents_kmp(self, document_texts: Dict[str, str], pattern: str, relevant_docs: List[str] = None) -> Dict:
+        results = {}
+        total_matches = 0
+        docs_to_search = relevant_docs if relevant_docs is not None else list(document_texts.keys())
+        
+        for doc_name in docs_to_search:
+            if doc_name in document_texts:
+                doc_result_kmp = self.get_results_kmp(document_texts[doc_name], pattern)
+                results[doc_name] = doc_result_kmp # inserting into dict
+                total_matches += doc_result_kmp['matches_count']  
+           
+        return {
+            'pattern': pattern,
+            'total_matches': total_matches,
+            'documents_found': len(results),
+            'document_results': results
+        }
+    
+    def search_in_documents_bruteforce(self, document_texts: Dict[str, str], pattern: str, relevant_docs:List[str] = None) -> Dict:
+        results = {}
+        total_matches = 0
+        docs_to_search = relevant_docs if relevant_docs is not None else list(document_texts.keys())
+        
+        for doc_name in docs_to_search:
+            if doc_name in document_texts:
+                doc_result_brtfrc = self.get_results_bruteforce(document_texts[doc_name], pattern)
+                results[doc_name] = doc_result_brtfrc # inserting into dict
+                total_matches += doc_result_brtfrc['matches_count']  
+           
+        return {
+            'pattern': pattern,
+            'total_matches': total_matches,
+            'documents_found': len(results),
+            'document_results': results
         }
